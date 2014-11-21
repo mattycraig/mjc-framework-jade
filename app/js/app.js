a38251c6fbebf59dd5f6a34cdf201ee47700e335
Matthew Craig <matthew.craig@oneadvertising.ca>
2014-11-21 14:53:49 -0500

    Initial commit
    Use Jade templating engine to process and build out HTML files.

---
+++ app/js/app.js
@@ -0,0 +1,58 @@
+/* global outdatedBrowser, FastClick */
+
+(function ($, window, document, undefined) {
+
+	'use strict';
+
+	//--------------------------------------------|
+	// AVOID CONSOLE ERRORS
+	//--------------------------------------------|
+	$(function () {
+		var method;
+		var noop = function () {};
+		var methods = [
+			'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+			'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+			'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+			'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+		];
+		var length = methods.length;
+		var console = (window.console = window.console || {});
+
+		while (length--) {
+			method = methods[length];
+
+			// Only stub undefined methods.
+			if (!console[method]) {
+				console[method] = noop;
+			}
+		}
+	});
+
+	//--------------------------------------------|
+	// OUTDATED BROWSER
+	//--------------------------------------------|
+	$(function () {
+		outdatedBrowser({
+			bgColor: '#f25648',
+			color: '#ffffff',
+			lowerThan: 'boxShadow',
+			languagePath: ''
+		});
+	});
+
+	//--------------------------------------------|
+	// FASTCLICK.JS
+	//--------------------------------------------|
+	$(function () {
+    	FastClick.attach(document.body);
+	});
+
+	//--------------------------------------------|
+	// GO!
+	//--------------------------------------------|
+	$(function () {
+		console.log('Lets get started!');
+	});
+
+})(jQuery, window, document);
